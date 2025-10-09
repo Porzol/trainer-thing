6c59bf57f8eb5d8ae19063ab9b877b716163a209
David Matthew Antonio <i_davidmatthew.antonio@stratpoint.com>
2025-10-10 01:29:55 +0800

    Added mount script again

--- mount.sh
+++ mount.sh
@@ -1,5 +1,7 @@
 mkdir -p /mnt/vdb
-mount /dev/vdb /mnt/vdb
+if ! mountpoint -q /mnt/vdb; then
+    mount /dev/vdb /mnt/vdb
+fi
 mkdir -p dataset/
 ln -s /mnt/vdb/Cam2 dataset/ 
 ln -s /mnt/vdb/Cam4 dataset/ 
